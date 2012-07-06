274abc3e33ae722a2b39cef2fc78e22d90728765
uccmen <ucc.men@gmail.com>
2012-07-06 13:49:17 -0300

    Update master

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <?php
 include 'facebook.php';
-$app_id = "INSERT APP ID HERE";
-$app_secret = "INSERT APP SECRET HERE";
+$app_id = "194472553967386";
+$app_secret = "c1c717bae764f6942cab276638886fd0";
 $facebook = new Facebook(array(
     'appId' => $app_id,
     'secret' => $app_secret,
@@ -10,110 +10,143 @@ $facebook = new Facebook(array(
 
 $user = $facebook->getUser();
 
-if ($user <> '0' && $user <> '') { /*if valid user id i.e. neither 0 nor blank nor null*/
-try {
-// Proceed knowing you have a logged in user who's authenticated.
-$user_profile = $facebook->api('/me');
-} catch (FacebookApiException $e) { /*sometimes it shows user id even if user in not logged in and it results in Oauth exception. In this case we will set it back to 0.*/
-error_log($e);
-$user = '0';
+
+if ($user <> '0' && $user <> '') 
+{ 
+    $user_profile = $facebook->api('/me');
+	$fbname = $user_profile['name'];
 }
+else
+{
+	$loginUrl = $facebook->getLoginUrl();
+	header('Location: ' . $loginUrl);
 }
 
-
-if ($user <> '0' && $user <> '') { /*So now we will have a valid user id with a valid oauth access token and so the code will work fine.*/
-$user_profile = $facebook->api('/me');
-echo "<p>Welcome : " . $user_profile['name'];
-echo "<p>";
-echo "Your FB ID is: " . $user;
-} else { ?>
-      <fb:login-button></fb:login-button>
-    <?php } ?>
-
-<head>
-
-
-    <script>
-
-
-      window.fbAsyncInit = function() {
-        FB.init({
-          appId      : '<?= YOUR_APP_ID ?>',
-          status     : true,
-          cookie     : true,
-          xfbml      : true
-        });
-
-        FB.Event.subscribe('auth.login', function(response) {
-          window.location.reload();
-        });
-      };
-
-
-      function sendRequestViaMultiFriendSelector() {
-        FB.ui({method: 'apprequests',
-          message: 'My Great Request'
-        }, requestCallback);
-      }
-
-
-      (function(d){
-         var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-         js = d.createElement('script'); js.id = id; js.async = true;
-         js.src = "//connect.facebook.net/en_US/all.js";
-         d.getElementsByTagName('head')[0].appendChild(js);
-       }(document));
-    </script>
-
-
-</body>
-</html>
-
-<?php
-$host="INSERT HOST NAME"; // Host name
-$username="INSERT USERNAME"; // Mysql username
-$password="INSERT PASSWORD"; // Mysql password
-$db_name="INSERT DATABASE NAME"; // Database name
-$tbl_name="INSERT TABLE NAME"; // Table name
+$host="stevenswallcom.ipagemysql.com"; // Host name
+$username="socialqs"; // Mysql username
+$password="323232"; // Mysql password
+$db_name="socialqs_db"; // Database name
+$tbl_name="socialqs_question"; // Table name
 
 // Connect to server and select databse.
 mysql_connect("$host", "$username", "$password")or die("cannot connect");
 mysql_select_db("$db_name")or die("cannot select DB");
 
-$sqla="SELECT * FROM $tbl_name ORDER BY datetime DESC LIMIT 10";
+$sqla="SELECT * FROM $tbl_name ORDER BY id DESC LIMIT 10";
 // OREDER BY datetime DESC is order result by descending
 $result=mysql_query($sqla);
-?>
 
-<form action="add_question.php" method="post" align="center">
-<input type="text" name="question" id="question" style="width:1000px; height:100px; font-size:35"/>
-<input type="submit" value="Ask !" style="height: 50px; width: 350px; font-size:32"/>
+
+
+
+
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"
+  xmlns:fb="https://www.facebook.com/2008/fbml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <title>Social Knowledge Homepage</title>
+</head>
+
+
+
+    <body>
+	<div id="fb-root"></div>
+	<script src="http://connect.facebook.net/en_US/all.js"></script>
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '194472553967386',
+          channelUrl : 'http://www.stevenswall.com/fbtest/channel.html', // Channel File
+          status     : true,
+          cookie     : true,
+          xfbml      : true
+        });
+
+
+      FB.getLoginStatus(function(response) {
+	  		    if (response.status === 'connected') {
+	  		      var uid = response.authResponse.userID;
+	  		      var accessToken = response.authResponse.accessToken;
+
+	  		    } else if (response.status === 'not_authorized') {
+	  		      // the user is logged in to Facebook,
+	  		      //but not connected to the app
+	  		      window.location = "http://apps.facebook.com/socialqs";
+	  		    } else {
+	  		      window.location = "http://stevenswall.com/fbtest/index.php";
+	  		    }
+ });
+
+
+
+      <script>(function(d){
+         var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+         js = d.createElement('script'); js.id = id; js.async = true;
+         js.src = "//connect.facebook.net/en_US/all.js";
+         d.getElementsByTagName('head')[0].appendChild(js);
+       }(document));</script>
+
+
+<link rel="stylesheet" type="text/css" media="all" href="tables.css" />
+<link rel="stylesheet" type="text/css" media="all" href="center.css" />
+<link rel="stylesheet" href="form.css" type="text/css" media="all" />
+<form class="form" action="add_question.php" method="post">
+
+<p class="question">
+<input type="text" name="question" id="question" style="width:790px; height:100px; font-size:35"/>
+<label for="question">Question</label>
+</p>
+
+<p class="submit" align="left">
+<input type="submit" value="Ask" style="height: 50px; width: 250px; font-size:32"/>
+</p>
 </form>
 
-<table width="90%" border="0" align="center" cellpadding="3" cellspacing="1" bgcolor="#CCCCCC">
+
+<center>
+<table id="rounded-corner">
+<thead>
+
 <tr>
-<td colspan="6" align="center" bgcolor="#E6E6E6"><strong><font size=16>MOST RECENT ASKED QUESTIONS (TOP 10)</font></strong></td>
+<th scope="col" class="rounded-company" colspan="7"><strong><p class="table">RECENTLY ASKED QUESTIONS (TOP 10)</p></strong></th>
+<th scope="col" class="rounded-q4"></th>
 </tr>
+
 <tr>
-<td width="6%" align="center" bgcolor="#E6E6E6"><strong>FB ID</strong></td>
-<td width="4%" align="center" bgcolor="#E6E6E6"><strong>ID</strong></td>
-<td width="53%" align="center" bgcolor="#E6E6E6"><strong>Question</strong></td>
-<td width="6%" align="center" bgcolor="#E6E6E6"><strong>Views</strong></td>
-<td width="6%" align="center" bgcolor="#E6E6E6"><strong>Replies</strong></td>
-<td width="13%" align="center" bgcolor="#E6E6E6"><strong>Date/Time</strong></td>
+<th scope="col" class="rounded-q1" align="center"><strong>User</strong></th>
+<th scope="col" class="rounded-q2" align="left"><strong>Name</strong></th>
+<th scope="col" class="rounded-q3" align="left"><strong>Question</strong></th>
+<th scope="col" class="rounded-q1" align="center"><strong>Views</strong></th>
+<th scope="col" class="rounded-q2" align="center"><strong>Replies</strong></th>
+<th scope="col" class="rounded-q2" align="center"><strong>Likes</strong></th>
+<th scope="col" class="rounded-q2" align="center"><strong>Shares</strong></th>
+<th scope="col" class="rounded-q3" align="center"><strong>Date/Time</strong></th>
 </tr>
+</thead>
 
 <?php
-while($rows=mysql_fetch_array($result)){ // Start looping table row
+
+
+while($rows = mysql_fetch_array($result))
+{ 
+// Start looping table row
+
+
 ?>
 
+<tbody>
 <tr>
-<td bgcolor="#FFFFFF"><? echo $rows['fbid']; ?></td>
-<td bgcolor="#FFFFFF"><? echo $rows['id']; ?></td>
-<td bgcolor="#FFFFFF"><a href="view_question.php?id=<? echo $rows['id']; ?>"><? echo $rows['question']; ?></a><BR></td>
-<td align="center" bgcolor="#FFFFFF"><? echo $rows['view']; ?></td>
-<td align="center" bgcolor="#FFFFFF"><? echo $rows['reply']; ?></td>
-<td align="center" bgcolor="#FFFFFF"><? echo $rows['datetime']; ?></td>
+<td align="center"><a target="_blank" href="http://www.facebook.com/<? echo $rows['fbid']; ?>"><img src="https://graph.facebook.com/<? echo $rows['fbid']; ?>/picture"></a></td>
+<td><? echo $rows['name']; ?></td>
+<td><a href="view_question.php?id=<? echo $rows['id']; ?>"><? echo $rows['question']; ?></a></td>
+<td align="center"><? echo $rows['view']; ?></td>
+<td align="center"><? echo $rows['replies']; ?></td>
+<td align="center"><? echo $rows['likes']; ?></td>
+<td align="center"><? echo $rows['shares']; ?></td>
+<td align="center"><? echo $rows['datetime']; ?></td>
 </tr>
 
 <?php
@@ -121,7 +154,20 @@ while($rows=mysql_fetch_array($result)){ // Start looping table row
 }
 mysql_close();
 ?>
-<tr>
-<td colspan="6" align="right" bgcolor="#E6E6E6"><a href="all_questions.php"><strong>View All Questions</strong> </a></td>
-</tr>
+<tfoot>
+
+    	<tr>
+
+        	<td colspan="7" class="rounded-foot-left"><em><a href="all_questions.php"><strong>View All Questions</strong> </a></em></td>
+
+        	<td class="rounded-foot-right">&nbsp;</td>
+
+        </tr>
+
+    </tfoot>
+
+
+</tbody>
 </table>
+</center>
+</html>
