6d322fd69889ab1fde4d72fa15528ec9ffaf4e9e
uccmen <ucc.men@gmail.com>
2012-07-06 13:51:58 -0300

    Update master

--- index.php
+++ index.php
@@ -1,36 +1,60 @@
-<?php
-include 'facebook.php';
-$app_id = "INSERT APP ID HERE";
-$app_secret = "INSERT APP SECRET HERE";
-$facebook = new Facebook(array(
-    'appId' => $app_id,
-    'secret' => $app_secret,
-    'cookie' => true
-));
-
-$user = $facebook->getUser();
-
-if ($user <> '0' && $user <> '') { /*if valid user id i.e. neither 0 nor blank nor null*/
-try {
-// Proceed knowing you have a logged in user who's authenticated.
-$user_profile = $facebook->api('/me');
-} catch (FacebookApiException $e) { /*sometimes it shows user id even if user in not logged in and it results in Oauth exception. In this case we will set it back to 0.*/
-error_log($e);
-$user = '0';
-}
-}
-if ($user <> '0' && $user <> '') { /*So now we will have a valid user id with a valid oauth access token and so the code will work fine.*/
-echo "<center>Welcome UserId : " . $user;
-
-$user_profile = $facebook->api('/me');
-echo "<p><center>You must be " . $user_profile['name'];
-echo "<p>";
+ <html>
+    <head>
+      <title>Social Knowledge Login Page</title>
+    </head>
+    <link rel="stylesheet" type="text/css" media="all" href="center.css" />
+    <body>
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            appId      : '194472553967386',
+            channelUrl : 'http://www.stevenswall.com/fbtest/channel.html', // Channel File
+            status     : true,
+            cookie     : true,
+            xfbml      : true,
+            oauth      : true,
+          });
 
 
-}
 
-?>
+          FB.getLoginStatus(function(response) {
+    	    if (response.status === 'connected') {
+		      var uid = response.authResponse.userID;
+		      var accessToken = response.authResponse.accessToken;
+		      window.location = "http://stevenswall.com/fbtest/main.php";
+		    } else if (response.status === 'not_authorized') {
+		      // the user is logged in to Facebook,
+		      //but not connected to the app
+		      window.location = "http://apps.facebook.com/socialqs";
+		    } else {
+		      // the user isn't even logged in to Facebook.
 
-<form action="main.php" method="post" align="center">
-<input type="submit" value="Click To Proceed..." style="height: 50px; width: 350px; font-size:32"/>
-</form>
+		    }
+ });
+
+
+
+            FB.Event.subscribe('auth.login', function(response) {
+		            window.location.reload();
+		            window.location = "http://stevenswall.com/fbtest/main.php";
+        });
+
+
+        };
+        (function(d){
+           var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+           js = d.createElement('script'); js.id = id; js.async = true;
+           js.src = "//connect.facebook.net/en_US/all.js";
+           d.getElementsByTagName('head')[0].appendChild(js);
+         }(document));
+      </script>
+	  <table id="outer" cellpadding="0" cellspacing="0" height="10%">
+  		<tr><td id="middle">
+	  <p>Welcome to Social Knowledge !</p>
+      <div align="center" class="fb-login-button">Login with Facebook</div>
+        </td></tr>
+ 	  </table>
+
+    </body>
+ </html>
